680f11d193da7fe6ed7314fdf746766691d85a29
Sam Shen <42982732+shen420@users.noreply.github.com>
2021-03-23 14:46:40 -0700

    Add files via upload

--- R/ISFragfunctions_20201229.R
+++ R/ISFragfunctions_20201229.R
@@ -1,77 +1,7 @@
-#Functions--------------------------------------------------------------------------------------------
+#ISFrag Functions--------------------------------------------------------------------------------------------
 
-#Reverse dot product (x -> y, y as a template)
-reverse_dp <- function(x,y){ # x: experimental MS2, y: reference MS2
-  x <- x[x[,2]>0,]
-  y <- y[y[,2]>0,]
-  if(nrow(x)==0 | nrow(y)==0){return(0)}
-  x[,2] <- 100*x[,2]/max(x[,2])
-  y[,2] <- 100*y[,2]/max(y[,2])
-  alignment <- data.frame(matrix(nrow=nrow(x), ncol=3))
-  alignment[,1:2] <- x[,1:2]
-  y1 <- y  # in case one row in y can be selected multiple times
-  for(i in 1:nrow(x)){
-    mass.diff <- abs(y1[,1] - x[i,1])
-    if(min(mass.diff) <= ms2.tol){
-      alignment[i,3] <- y1[mass.diff==min(mass.diff),2][1]
-      y1[mass.diff==min(mass.diff),1][1] <- NA   # after matched, NA assigned
-      y1 <- y1[complete.cases(y1),]
-      if(is.null(nrow(y1)) ==TRUE) break
-      if(nrow(y1)==0) break
-    }
-  }
-  alignment <- alignment[complete.cases(alignment),]
-  if(nrow(alignment)==0){score <- 0}
-  if(nrow(alignment)>0){
-    #dot product calculation
-    AB <- sum(alignment[,2]*alignment[,3])
-    A <- sum(alignment[,2]^2)
-    B <- sum(y[,2]^2)
-    score <- as.numeric(AB/sqrt(A*B))
-  }
-  match_No <- nrow(alignment)
-  matched_ratio <- as.numeric(nrow(alignment)/nrow(y))
-  return  <- c(score,match_No,matched_ratio)
-  return(return)
-}
-
-#Single sample MS2 matching
-matchMS2 <- function(x, featuretable, expandRt = 0, expandMz = 0, ppm = 0) {
-  pks <- featuretable
-  if (ppm != 0){
-    mz.diff <- pks[, "mz"] * ppm / 1e6
-    expandMz <- rep(expandMz, nrow(pks))
-  }else{
-    mz.diff <- rep(0, nrow(pks))
-    expandMz <- rep(expandMz, nrow(pks))
-  }
-  pks[, "mzmin"] <- pks[, "mz"] - expandMz - mz.diff
-  pks[, "mzmax"] <- pks[, "mz"] + expandMz + mz.diff
-  pks[, "rtmin"] <- pks[, "rt"] - expandRt
-  pks[, "rtmax"] <- pks[, "rt"] + expandRt
-
-  peak_ids <- rownames(pks)
-  fromFile <- 1L
-  sps <- spectra(x)
-  pmz <- precursorMz(x)
-  rtm <- rtime(x)
-  res <- vector(mode = "list", nrow(pks))
-  for (i in 1:nrow(pks)) {
-    if (is.na(pks[i, "mz"])) next()
-    idx <- which(pmz >= pks[i, "mzmin"] & pmz <= pks[i, "mzmax"] &
-                   rtm >= pks[i, "rtmin"] & rtm <= pks[i, "rtmax"])
-    if (length(idx)) {
-      res[[i]] <- lapply(sps[idx], function(z) {
-        z
-      })
-    }
-  }
-  names(res) <- peak_ids
-  return(res)
-}
-
-#Multi sample MS2 matching
-matchMS2multi <- function(x,featuretable, expandRt = 0, expandMz = 0, ppm = 0) {
+#Helper function: match ms2 to features
+matchMS2 <- function(x,featuretable, expandRt = 0, expandMz = 0, ppm = 0) {
   pks <- featuretable
   pks <- cbind(pks, 0)
   colnames(pks)[ncol(pks)] <- "mzmin"
@@ -108,34 +38,10 @@ matchMS2multi <- function(x,featuretable, expandRt = 0, expandMz = 0, ppm = 0) {
   return(res)
 }
 
-#EIC peak smoothing
-peak_smooth <- function(x,level=2){
-  n <- level
-  if(length(x) < 2*n){
-    return(x)
-  } else if(length(unique(x))==1){
-    return(x)
-  } else{
-    y <- vector(length=length(x))
-    for(i in 1:n){
-      y[i] <- sum(c((n-i+2):(n+1),n:1)*x[1:(i+n)])/sum(c((n-i+2):(n+1),n:1))
-    }
-    for(i in (n+1):(length(y)-n)){
-      y[i] <-  sum(c(1:(n+1),n:1)*x[(i-n):(i+n)])/sum(c(1:(n+1),n:1))
-    }
-    for(i in (length(y)-n+1):length(y)){
-      y[i] <- sum(c(1:n,(n+1):(n+i-length(x)+1))*x[(i-n):length(x)])/sum(c(1:n,(n+1):(n+i-length(x)+1)))
-    }
-    return(y)
-  }
-}
-
-#Feature Detection
-generate.featuretable <- function(MS1directory, MS2directory, type, ppm=10, peakwidth=c(10,120), mzdiff = 0.01,
+#Feature detection using XCMS
+generate.featuretable <- function(MS1directory, type, ppm=10, peakwidth=c(10,120), mzdiff = 0.01,
                               snthresh = 6, integrate = 1, prefilter = c(3,100), noise = 100, bw = 5,
                               mzwid = 0.015, max = 100, CAMERA = F){
-
-  # This script is to annotate in-source-fragmentation
 
   #XCMS feature detection
   cwp <- CentWaveParam(ppm = ppm,
@@ -153,13 +59,11 @@ generate.featuretable <- function(MS1directory, MS2directory, type, ppm=10, peak
     MS1data <- findChromPeaks(MS1data, param = cwp)
     data_filtered <- filterMsLevel(MS1data, msLevel = 1L)
     xset <- as(data_filtered, 'xcmsSet')
-    setwd(MS2directory)
-    MS2.files <- list.files(pattern = ".mzXML")
-    MS2data <- readMSData(MS2.files, mode = "onDisk")
 
     featureTable <- as.data.frame(xset@peaks)
     featureTable <- featureTable[order(featureTable$mz, decreasing = TRUE ),]
     rownames(featureTable) <- paste("F", 1:nrow(featureTable), sep="")
+    featureTable <- featureTable[, c("mz", "rt", "rtmin", "rtmax", "maxo")]
 
     if(CAMERA){
       xsa<-xsAnnotate(xset)
@@ -184,9 +88,6 @@ generate.featuretable <- function(MS1directory, MS2directory, type, ppm=10, peak
     MS1data <- findChromPeaks(MS1data, param = cwp, SnowParam())
     data_filtered <- filterMsLevel(MS1data, msLevel = 1L)
     xset <- as(data_filtered, 'xcmsSet')
-    setwd(MS2directory)
-    MS2.files <- list.files(pattern = ".mzXML")
-    MS2data <- readMSData(MS2.files, mode = "onDisk")
 
     xset <- group(xset, bw = bw, minfrac = 0.5, mzwid = mzwid, minsamp = 1, max = max)
     xset <- retcor(xset, method = "obiwarp", profStep = 1)
@@ -217,95 +118,199 @@ generate.featuretable <- function(MS1directory, MS2directory, type, ppm=10, peak
     }
   }
   MS1.files <<- MS1.files
-  MS2.files <<- MS2.files
   MS1data <<- MS1data
-  MS2data <<- MS2data
   return(featureTable)
 }
 
+#Add additional features (CSV) to XCMS feature table
+add.features <- function(ft_directory, ft_name, featureTable = NA){
+  setwd(ft_directory)
+  addFT <- read.csv(ft_name, header = T, stringsAsFactors = F)
+
+  if(is.na(featureTable)){
+    colnames(addFT)[1:4] <- c("mz", "rt", "rtmin", "rtmax")
+    featureTable <- addFT
+  }else{
+    colnames(addFT) <- colnames(featureTable)
+    featureTable <- rbind(featureTable, addFT)
+  }
+
+  dereplicatedFT <- data.frame(matrix(ncol = ncol(featureTable), nrow = 0)) #generate data frame with dereplicated features
+  colnames(dereplicatedFT) <- colnames(featureTable)
+  for(m in (1:nrow(featureTable))) {
+    mass.lower.limit <- featureTable$mz[m] - 0.01
+    mass.upper.limit <- featureTable$mz[m] + 0.01
+    rt.lower.limit <- featureTable$rt[m] - 30
+    rt.upper.limit <- featureTable$rt[m] + 30
+    temp <- dereplicatedFT[dereplicatedFT$mz >= mass.lower.limit & dereplicatedFT$mz <= mass.upper.limit,]
+    temp <- temp[temp$rt >= rt.lower.limit & temp$rt <= rt.upper.limit,]
+    if(nrow(temp) == 0) {
+      dereplicatedFT[nrow(dereplicatedFT) + 1,] = featureTable[m,]
+    }else{
+      index <- which(dereplicatedFT$mz == temp$mz[1])[1]
+      if(sum(dereplicatedFT[index, (5:ncol(featureTable))]) < temp[1, (5:ncol(featureTable))]){
+        dereplicatedFT[index, ] <- temp[1, ]
+      }
+    }
+  }
+
+  featureTable <- dereplicatedFT
+  featureTable <- featureTable[order(featureTable$mz, decreasing = TRUE ),]
+  rownames(featureTable) <- paste("F", 1:nrow(featureTable), sep="")
+  return(featureTable)
+}
 
 #MS2 Assignment
-ms2.tofeaturetable <- function(MS2data, featureTable, type){
-  if(type == "single"){
-    MS2spectra <- matchMS2(MS2data, featureTable, expandRt = 10, expandMz = 0.01, ppm = 0)
-    featureTable <- cbind(featureTable,F,0,0,0,0)
-    colnames(featureTable)[(ncol(featureTable)-4):ncol(featureTable)] <- c("MS2_match", "MS2mz", "MS2int",
-                                                                           "PeaksCount", "fromFile")
-    featureTable <- cbind(featureTable, 0)
-    colnames(featureTable)[ncol(featureTable)] <- "ISF_level"
+ms2.tofeaturetable <- function(MS2directory, featureTable){
+  setwd(MS2directory)
+  MS2.files <- list.files(pattern = ".mzXML")
+  MS2data <- readMSData(MS2.files, mode = "onDisk")
 
-    for (i in 1:nrow(featureTable)) {
-      if(!is.null(MS2spectra[[i]])){
-        tmpSpectra <- MS2spectra[[i]]
-        for (j in 1:length(tmpSpectra)){
-          if(tmpSpectra[[j]]@peaksCount == 0){
-            tmpSpectra[[j]] <- NA
-          }
-        }
-        tmpSpectra <- tmpSpectra[is.na(tmpSpectra)==FALSE]
-        if(length(tmpSpectra) > 0){
-          currInt = tmpSpectra[[1]]@precursorIntensity
-          currIdx = 1
-          for(k in 1:length(tmpSpectra)){
-            if(tmpSpectra[[k]]@precursorIntensity > currInt){
-              currIdx = k
-              currInt = tmpSpectra[[k]]@precursorIntensity
-            }
-          }
-          finalSpectra = tmpSpectra[[currIdx]]
-          indices <- finalSpectra@intensity >= 1000;
-          finalSpectra@intensity <- finalSpectra@intensity[indices];
-          finalSpectra@mz <- finalSpectra@mz[indices];
-          featureTable$MS2_match[i] <- TRUE
-          featureTable$MS2mz[i] <- paste(round(finalSpectra@mz,4),collapse = ";")
-          featureTable$MS2int[i] <- paste(finalSpectra@intensity, collapse = ";")
-          featureTable$PeaksCount[i] <- finalSpectra@peaksCount
-          featureTable$fromFile[i] <- finalSpectra@fromFile
+  MS2spectra <- matchMS2(MS2data, featureTable, expandRt = 10, expandMz = 0.01, ppm = 0)
+  featureTable <- cbind(featureTable,F,0,0,0,0)
+  colnames(featureTable)[(ncol(featureTable)-4):ncol(featureTable)] <- c("MS2_match", "MS2mz", "MS2int",
+                                                                         "PeaksCount", "fromFile")
+  featureTable <- cbind(featureTable, 0)
+  colnames(featureTable)[ncol(featureTable)] <- "ISF_level"
+
+  for (i in 1:nrow(featureTable)) {
+    if(!is.null(MS2spectra[[i]])){
+      tmpSpectra <- MS2spectra[[i]]
+      for (j in 1:length(tmpSpectra)){
+        if(tmpSpectra[[j]]@peaksCount == 0){
+          tmpSpectra[[j]] <- NA
         }
       }
-    }
-
-  } else{
-    MS2spectra <- matchMS2multi(MS2data, featureTable, expandRt = 10, expandMz = 0.01, ppm = 0)
-    featureTable <- cbind(featureTable,F,0,0,0,0)
-    colnames(featureTable)[(ncol(featureTable)-4):ncol(featureTable)] <- c("MS2_match", "MS2mz", "MS2int",
-                                                                           "PeaksCount", "fromFile")
-    featureTable <- cbind(featureTable, 0)
-    colnames(featureTable)[ncol(featureTable)] <- "ISF_level"
-
-    for (i in 1:nrow(featureTable)) {
-      if(!is.null(MS2spectra[[i]])){
-        tmpSpectra <- MS2spectra[[i]]
-        for (j in 1:length(tmpSpectra)){
-          if(tmpSpectra[[j]]@peaksCount == 0){
-            tmpSpectra[[j]] <- NA
+      tmpSpectra <- tmpSpectra[is.na(tmpSpectra)==FALSE]
+      if(length(tmpSpectra) > 0){
+        currInt = tmpSpectra[[1]]@precursorIntensity
+        currIdx = 1
+        for(k in 1:length(tmpSpectra)){
+          if(tmpSpectra[[k]]@precursorIntensity > currInt){
+            currIdx = k
+            currInt = tmpSpectra[[k]]@precursorIntensity
           }
         }
-        tmpSpectra <- tmpSpectra[is.na(tmpSpectra)==FALSE]
-        if(length(tmpSpectra) > 0){
-          currInt = tmpSpectra[[1]]@precursorIntensity
-          currIdx = 1
-          for(k in 1:length(tmpSpectra)){
-            if(tmpSpectra[[k]]@precursorIntensity > currInt){
-              currIdx = k
-              currInt = tmpSpectra[[k]]@precursorIntensity
-            }
-          }
-          finalSpectra = tmpSpectra[[currIdx]]
-          indices <- finalSpectra@intensity >= 1000;
-          finalSpectra@intensity <- finalSpectra@intensity[indices];
-          finalSpectra@mz <- finalSpectra@mz[indices];
-          featureTable$MS2_match[i] <- TRUE
-          featureTable$MS2mz[i] <- paste(round(finalSpectra@mz,4),collapse = ";")
-          featureTable$MS2int[i] <- paste(finalSpectra@intensity, collapse = ";")
-          featureTable$PeaksCount[i] <- finalSpectra@peaksCount
-          featureTable$fromFile[i] <- finalSpectra@fromFile
-        }
+        finalSpectra = tmpSpectra[[currIdx]]
+        indices <- finalSpectra@intensity >= 1000;
+        finalSpectra@intensity <- finalSpectra@intensity[indices];
+        finalSpectra@mz <- finalSpectra@mz[indices];
+        featureTable$MS2_match[i] <- TRUE
+        featureTable$MS2mz[i] <- paste(round(finalSpectra@mz,4),collapse = ";")
+        featureTable$MS2int[i] <- paste(finalSpectra@intensity, collapse = ";")
+        featureTable$PeaksCount[i] <- finalSpectra@peaksCount
+        featureTable$fromFile[i] <- finalSpectra@fromFile
       }
     }
-
   }
   featureTable[featureTable == ""] <- 0
+  return(featureTable)
+}
+
+#Feature Annotation
+feature.annotation <- function(featureTable, lib_directory, lib_name, dp = 0.7, ms1.tol = 0.01, ms2.tol = 0.02){
+  # Dot product function
+  dp.score <- function(x,y){
+    if(nrow(x)==0 | nrow(y)==0){return(0)}
+    x[,2] <- 100*x[,2]/max(x[,2])
+    y[,2] <- 100*y[,2]/max(y[,2])
+    alignment <- data.frame(matrix(nrow=nrow(x), ncol=3))
+    alignment[,1:2] <- x[,1:2]
+    y1 <- y  ##in case one row in y can be selected multiple times
+    for(i in 1:nrow(x)){
+      mass.diff <- abs(y1[,1] - x[i,1])
+      if(min(mass.diff) <= ms2.tol){
+        alignment[i,3] <- y1[mass.diff==min(mass.diff),2][1]
+        y1[mass.diff==min(mass.diff),1][1] <- NA   # after matched, NA assigned
+        y1 <- y1[complete.cases(y1),]
+        if(is.null(nrow(y1)) ==TRUE) break
+        if(nrow(y1)==0) break
+      }
+    }
+    alignment <- alignment[complete.cases(alignment),]
+    if(nrow(alignment)==0){score <- 0}
+    if(nrow(alignment)>0){
+      #dot product calculation
+      AB <- sum(alignment[,2]*alignment[,3])
+      A <- sum(x[,2]^2)
+      B <- sum(y[,2]^2)
+      dp.score <- AB/sqrt(A*B)
+      score <- as.numeric(dp.score)
+    }
+    match_No <- nrow(alignment)
+    return  <- c(score,match_No)
+    return(return)
+  }
+
+  # Calculate the number of cores
+  no_cores <- detectCores() - 1
+  print("Using cores:")
+  print(no_cores)
+  # Initiate cluster
+  registerDoParallel(no_cores)
+
+  setwd(lib_directory)
+  database <- read.msp(lib_name, only.org = FALSE)
+  featureTable <- cbind(featureTable, 0)
+  colnames(featureTable)[ncol(featureTable)] <- "Annotation"
+  featureTable <- cbind(featureTable, 0)
+  colnames(featureTable)[ncol(featureTable)] <- "DPscore"
+
+  rez <- foreach(x = 1:nrow(featureTable)) %dopar% {
+    premass.Q <- featureTable$mz[x]     ###query precursor ion mass
+
+    if(featureTable$MS2mz[x] == 0){
+      # featureTable$Annotation[x] <- "unknown"
+      return(c("unknown", 0))
+    }
+
+    ms2.Q <- data.frame(m.z = strsplit(featureTable$MS2mz[x], ";")[[1]],
+                        int = strsplit(featureTable$MS2int[x], ";")[[1]])  ###query ms2 input, ncol = 2, m.z & int
+    ms2.Q$m.z <- as.numeric(as.character(ms2.Q$m.z))
+    ms2.Q$int <- as.numeric(as.character(ms2.Q$int))
+
+    output <- data.frame(matrix(ncol=3))
+    colnames(output) <- c('std.name','DP.score','match_No')
+    h <- 1
+    for(i in 1:length(database)){
+      if(is.null(database[[i]]$PrecursorMZ)==TRUE) next # no precursor mass
+
+      premass.L <- database[[i]]$PrecursorMZ # database precursor
+      if(abs(premass.L-premass.Q) > ms1.tol) next # precursor filter
+
+      ms2.L <- as.data.frame(database[[i]]$pspectrum) # database spectrum
+      name.L <- database[[i]]$Name
+
+      output[h,1] <- name.L
+      output[h,2] <- dp.score(ms2.Q,ms2.L)[1]
+      output[h,3] <- dp.score(ms2.Q,ms2.L)[2]
+
+      h <- h + 1
+    }
+    output <- output[complete.cases(output),]
+
+    # Dp score threshold, Dp score >= 0.7 , match_No >= 6 (used in GNPS identification)
+    output <- output[output[,2] >= dp,]
+    # output <- output[output[,3] >= match.number.threshold,]
+
+    if(nrow(output)==0) {
+      # featureTable$Annotation[x] <- "unknown"
+      return(c("unknown", 0))
+    } else {
+      output <- output[order(-output[,2]),] # sort by scores
+      # featureTable$Annotation[x] <- output[1,1]
+      # featureTable$DPscore[x] <- output[1,2]
+      return(c(output[1,1], output[1,2]))
+    }
+  }
+
+  for(x in 1:nrow(featureTable)){
+    featureTable[x, c("Annotation", "DPscore")] <- rez[[x]]
+  }
+  featureTable$DPscore <- as.numeric(featureTable$DPscore)
+  #clean up the cluster
+  stopImplicitCluster()
+
   return(featureTable)
 }
 
@@ -336,7 +341,7 @@ find.level3 <- function(MS1directory, MS1.files, featureTable, type, peakCOR = 0
   }
 
   # Calculate the number of cores
-  no_cores <- detectCores() - 2
+  no_cores <- detectCores() - 1
   print("Using cores:")
   print(no_cores)
   # Initiate cluster
